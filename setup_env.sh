cdb5c373ed71e43d97e6165929f220351995e077
Patrick Spieler <stapelzeiger@gmail.com>
2015-10-08 22:46:32 +0200

    use upgrade option to setup python env

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 virtualenv --python=python3 env
-source env/bin/activate.fish 
-pip install -r requirements.txt
+source env/bin/activate.fish
+pip install --upgrade -r requirements.txt
