32c156ca5e460531370ef925d87db87eb3b61868
Florian Reinhard <florian.reinhard@epfl.ch>
2015-10-26 09:19:37 +0100

    waypoints test tool and config

--- tools/waypoint_send.py
+++ tools/waypoint_send.py
@@ -11,8 +11,9 @@ socket.connect("ipc://ipc/sink")
 
 x = float(sys.argv[1])
 y = float(sys.argv[2])
+theta = float(sys.argv[3])
 
 time.sleep(0.1)
-socket.send(msgpack.packb('wheelbase_waypoint') + msgpack.packb([x, y], use_single_float=True))
-socket.send(msgpack.packb('wheelbase_waypoint') + msgpack.packb([x, y], use_single_float=True))
-socket.send(msgpack.packb('wheelbase_waypoint') + msgpack.packb([x, y], use_single_float=True))
+socket.send(msgpack.packb('wheelbase_waypoint') + msgpack.packb([x, y, theta], use_single_float=True))
+socket.send(msgpack.packb('wheelbase_waypoint') + msgpack.packb([x, y, theta], use_single_float=True))
+socket.send(msgpack.packb('wheelbase_waypoint') + msgpack.packb([x, y, theta], use_single_float=True))
