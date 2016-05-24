0c694c34b457eff815cb33bb46c0f97664e2812b
Florian Reinhard <florian.reinhard@epfl.ch>
2016-05-25 00:16:41 +0200

    kalman position estimation with lidar and odometry

--- state_estimation/position.py
+++ state_estimation/position.py
@@ -3,6 +3,7 @@ import queue
 import time
 import numpy as np
 import math
+from math import sin, cos
 
 
 def periodic_error(angle):
@@ -14,36 +15,79 @@ def periodic_error(angle):
     return angle
 
 
+def map_odometry_to_table(odometry_pos, offset):
+    x = cos(offset[2]) * odometry_pos[0] - sin(offset[2]) * odometry_pos[1] + offset[0]
+    y = sin(offset[2]) * odometry_pos[0] + cos(offset[2]) * odometry_pos[1] + offset[1]
+    theta = odometry_pos[2] + offset[2]
+    return [x, y, theta]
+
+
+def compute_odometry_offset(table_pos, odometry_pos):
+    theta = table_pos[2] - odometry_pos[2]
+    x = table_pos[0] - (cos(theta) * odometry_pos[0] - sin(theta) * odometry_pos[1])
+    y = table_pos[1] - (sin(theta) * odometry_pos[0] + cos(theta) * odometry_pos[1])
+    return [x, y, theta]
+
+def kalman_state_propagation(P, Phi, Q):
+    """
+        P: state covariance
+        Phi: state transition jacobian
+        Q: state transition noise
+        returns P_k+1
+    """
+    return Phi * P * Phi.T + Q
+
+def kalman_measurement_update(x, P, z, h_of_x, H, R):
+    """
+        x: state
+        P: state covariance
+        z: measurement
+        h_of_x: measurement prediction
+        H: measurement prediction jacobian
+        R: measurement noise
+    """
+    S = H * P * H.T + R
+    K = P * H.T * np.linalg.inv(S)
+    P = (np.eye(len(x)) - K * H) * P
+    x = x + K * (z - h_of_x)
+    return x, P
+
+
+def get_robot_position_from_lidar(lidar):
+    lidar_heading = lidar[2]
+    robot_position = np.array(lidar[0:2]) - np.array([cos(lidar_heading), sin(lidar_heading)]) * 0.074
+    return [robot_position[0], robot_position[1], lidar_heading]
+
+
+
 class PositionEstimator():
 
     def __init__(self):
-        self.position_odometry = np.array([0.0, 0.0])
-        self.heading_odometry = 0
-        self.position_offset = np.array([0.0, 0.0])
-        self.heading_offset = 0
+        self.position_odometry = np.array([0.0, 0.0, 0.0])
+        self.x = np.matrix([0.0, 0.0, 0.0]) # position and heading offset
+        self.P = np.matrix(np.diagonal([0.1, 0.1, 0.1]))
 
     def update_odometry(self, odometry):
-        self.position_odometry = np.array(odometry[0:2])
-        self.heading_odometry = odometry[2]
+        self.position_odometry = np.array(odometry)
 
-    def reset(self, pos=[0, 0], heading=0):
-        self.position_offset = np.array(pos) - self.position_odometry
-        self.heading_offset = heading - self.heading_odometry
+    def reset(self, pos=[0, 0, 0]):
+        self.x = np.matrix(compute_odometry_offset(pos, self.position_odometry))
 
-    def update_lidar(self, lidar):
-        lidar_heading = lidar[2]
-        lidar_position = np.array(lidar[0:2]) - np.array([math.cos(lidar_heading), math.sin(lidar_heading)]) * 0.074
-        delta_position = lidar_position - self.position_odometry - self.position_offset
-        delta_heading = lidar_heading - self.heading_odometry - self.heading_offset
-        delta_heading = periodic_error(delta_heading)
-        if np.linalg.norm(delta_position) < 0.5 and abs(delta_heading) < 0.3:
-            self.position_offset += 0.2*delta_position
-            self.heading_offset += 0.3*delta_heading
+    def update_lidar(self, robot_position):
+        # state propagation x_k+1 = x_k + n
+        Q = np.matrix(np.diagonal([0.001, 0.001, 0.001]))
+        self.P = kalman_state_propagation(self.P, np.eye(3), Q)
+        # measurement update
+        z = np.matrix(robot_position)
+        h_of_x = np.matrix(map_odometry_to_table(self.position_odometry, self.x))
+        H = np.matrix([[1, 0, -sin(self.x[2])*self.position_odometry[0]-cos(self.x[2])*self.position_odometry[1]],
+                       [0, 1, cos(self.x[2])*self.position_odometry[0]-sin(self.x[2])*self.position_odometry[1]],
+                       [0, 0, 1]])
+        R = np.matrix(np.diagonal([0.03, 0.03, 0.05]))
+        self.x, self.P = kalman_measurement_update(self.x, self.P, z, h_of_x, H, R)
 
     def get_position(self):
-        pos = self.position_odometry + self.position_offset
-        heading = self.heading_odometry + self.heading_offset
-        return pos.tolist() + [periodic_error(heading)]
+        return map_odometry_to_table(self.position_odometry, self.x)
 
 
 def main():
@@ -65,7 +109,7 @@ def main():
             pass
         try:
             l = node.recv('/lidar/position', timeout=0)
-            pose.update_lidar(l)
+            pose.update_lidar(get_robot_position_from_lidar(l))
             node.publish('/position', pose.get_position())
         except queue.Empty:
             pass
---
+++ state_estimation/test_position.py
@@ -0,0 +1,51 @@
+import unittest
+import position
+from math import cos, sin, pi
+import numpy as np
+
+
+class TestPosition(unittest.TestCase):
+
+    def test_map_odometry_to_table(self):
+        odometry_pos = [1, 2, 3]
+        offset = [0.1, 0.2, 0.3]
+        table_pos = position.map_odometry_to_table(odometry_pos, offset)
+        self.assertAlmostEqual(cos(0.3) * 1 - sin(0.3) * 2 + 0.1, table_pos[0])
+        self.assertAlmostEqual(sin(0.3) * 1 + cos(0.3) * 2 + 0.2, table_pos[1])
+        self.assertAlmostEqual(3 + 0.3, table_pos[2])
+
+    def test_kalman_state_propagation(self):
+        P = np.matrix([[1, 2], [2, 4]])
+        Phi = np.matrix([[3, 2], [1, 0]])
+        Q =  np.matrix([[0.1, 0], [0, 0.2]])
+
+        np.testing.assert_almost_equal(Phi * P * Phi.T + Q,
+                                       position.kalman_state_propagation(P, Phi, Q))
+
+    def test_kalman_measurement_update(self):
+        x = np.matrix([[1], [2]])
+        z = np.matrix([1])
+        P = np.matrix([[1, 2], [2, 4]])
+        h_of_x = np.matrix([0.9])
+        H = np.matrix([1, 2])
+        R = np.matrix([0.1])
+
+        S = H * P * H.T + R
+        K = P * H.T * np.linalg.inv(S)
+        P_kp1 = (np.eye(len(x)) - K * H) * P
+        x_kp1 = x + K * (z - h_of_x)
+
+        x_test, P_test = position.kalman_measurement_update(x, P, z, h_of_x, H, R)
+        np.testing.assert_almost_equal(x_kp1, x_test)
+        np.testing.assert_almost_equal(P_kp1, P_test)
+
+    def test_get_robot_position_form_lidar(self):
+        lidar = [1, 1, pi/2]
+        robot_position = position.get_robot_position_from_lidar(lidar)
+        self.assertAlmostEqual([1, 1-0.074, pi/2], robot_position)
+
+    def test_compute_odometry_offset(self):
+        table_pos = [1, 2, 3]
+        odometry_pos = [4, 5, 6]
+        offset = position.compute_odometry_offset(table_pos, odometry_pos)
+        np.testing.assert_almost_equal(table_pos, position.map_odometry_to_table(odometry_pos, offset))
