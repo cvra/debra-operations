c165e15a7a9817a78c98610402855d6ccae09551
Patrick Spieler <stapelzeiger@gmail.com>
2016-05-28 11:17:01 +0200

    flash-motor-boards.sh: put pump board ids in comment

--- scripts/flash-motor-boards.sh
+++ scripts/flash-motor-boards.sh
@@ -2,3 +2,4 @@
 . ./env/bin/activate
 
 sudo env/bin/bootloader_flash -p /dev/ttyACM0 -a 0x08003800 --device-class motor-board-v1  -b bus/motor-control-firmware/build/motor-control-firmware.bin 20 21 29 31 32 22 23 24 25 27 28
+# 34 35 36 37 38 39 47 49
